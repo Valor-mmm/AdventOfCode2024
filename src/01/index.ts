5858ccd44477916af8d0b38b069d35f0ad497349
Valor-mmm <v_dev/github@slmail.me>
2024-12-01 20:49:42 +0100

    feat(day01): challenge 02

--- src/01/index.ts
+++ src/01/index.ts
@@ -9,6 +9,36 @@ export function determineDistance(a: number[], b: number[]): number {
   }, 0);
 }
 
+function countOccurrences(list: number[]): Map<number, number> {
+  const sortedB = list.sort();
+  const countedValues: Map<number, number> = new Map();
+  let currentValue: undefined | number = undefined;
+  let currentCount: number = 0;
+  sortedB.forEach((value) => {
+    if (currentValue === undefined) {
+      currentValue = value;
+      currentCount = 1;
+    } else if (currentValue !== value) {
+      countedValues.set(currentValue, currentCount);
+      currentValue = value;
+      currentCount = 1;
+    } else {
+      currentCount += 1;
+    }
+  });
+
+  return countedValues;
+}
+
+export function determineSimilarity(a: number[], b: number[]) {
+  const countedValues = countOccurrences(b);
+
+  return a.reduce((accumulator, currentValue) => {
+    const bCount = countedValues.get(currentValue) ?? 0;
+    return accumulator + (bCount * currentValue);
+  }, 0);
+}
+
 interface Puzzle1Input {
   a: number[];
   b: number[];
@@ -33,6 +63,18 @@ function parseInput(path: string): Puzzle1Input {
 }
 
 if (import.meta.main) {
+  console.time("Parsing input");
   const input = parseInput("./src/01/puzzleInput");
+  console.timeEnd("Parsing input");
+
+  console.time("Calculating Distance");
   console.log("Distance between a & b = ", determineDistance(input.a, input.b));
+  console.timeEnd("Calculating Distance");
+
+  console.time("Calculating Similarity");
+  console.log(
+    "Similarity between a & b = ",
+    determineSimilarity(input.a, input.b),
+  );
+  console.timeEnd("Calculating Similarity");
 }
--- src/01/index_test.ts
+++ src/01/index_test.ts
@@ -1,9 +1,16 @@
 import { assertEquals } from "@std/assert";
-import { determineDistance } from "./index.ts";
+import { determineDistance, determineSimilarity } from "./index.ts";
 
 Deno.test(function distanceTest() {
   const a = [3, 4, 2, 1, 3, 3];
 
   const b = [4, 3, 5, 3, 9, 3];
   assertEquals(determineDistance(a, b), 11);
+});
+
+Deno.test(function similarityTest() {
+  const a = [3, 4, 2, 1, 3, 3];
+
+  const b = [4, 3, 5, 3, 9, 3];
+  assertEquals(determineSimilarity(a, b), 31);
 });
