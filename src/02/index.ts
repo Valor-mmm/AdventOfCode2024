d329985fd710a7a0cb970a75c17b5438d531438d
Valor-mmm <v_dev/github@slmail.me>
2024-12-02 07:29:57 +0100

    feat(day02): challenge 02

--- src/02/index.ts
+++ src/02/index.ts
@@ -4,32 +4,54 @@ function isDataSafe(previousData: number, data: number, increasing: boolean) {
   return correctDirection && Math.abs(difference) < 4;
 }
 
-export function determineSafety(reactorData: number[][]): number {
-  return reactorData.map((report) => {
-    let reportIsSafe = true;
-    let increasing: boolean | undefined;
-    let previousData: number | undefined;
+function isReportSafe(report: number[]) {
+  let reportIsSafe = true;
+  let increasing: boolean | undefined;
+  let previousData: number | undefined;
 
-    for (const data of report) {
-      // first iteration
-      if (previousData === undefined) {
-        previousData = data;
-        continue;
-      }
-
-      // second iteration
-      if (typeof increasing === "undefined") {
-        increasing = previousData < data;
-      }
-
-      if (!isDataSafe(previousData, data, increasing)) {
-        reportIsSafe = false;
-        break;
-      }
+  for (const data of report) {
+    // first iteration
+    if (previousData === undefined) {
       previousData = data;
+      continue;
     }
 
-    return reportIsSafe;
+    // second iteration
+    if (typeof increasing === "undefined") {
+      increasing = previousData < data;
+    }
+
+    if (!isDataSafe(previousData, data, increasing)) {
+      reportIsSafe = false;
+      break;
+    }
+    previousData = data;
+  }
+
+  return reportIsSafe;
+}
+
+export function determineSafety(reactorData: number[][]): number {
+  return reactorData.map((report) => isReportSafe(report)).filter(Boolean)
+    .length;
+}
+
+export function determineSafetyWithProblemDampener(
+  reactorData: number[][],
+): number {
+  return reactorData.map((report) => {
+    if (isReportSafe(report)) {
+      return true;
+    }
+
+    for (let i = 0; i < report.length; i++) {
+      const dampenedReport = report.toSpliced(i, 1);
+      if (isReportSafe(dampenedReport)) {
+        return true;
+      }
+    }
+
+    return false;
   }).filter(Boolean).length;
 }
 
@@ -54,7 +76,17 @@ if (import.meta.main) {
   const input = parseInput("./src/02/puzzleInput");
   console.timeEnd("Parsing input");
 
-  console.time("Calculating Reactor Data Safety");
-  console.log("Safety of reactor data: ", determineSafety(input.reactorData));
-  console.timeEnd("Calculating Reactor Data Safety");
+  console.time("Calculating Reactor Data Safety Without Problem Dampener");
+  console.log(
+    "Safety of reactor data without problem dampener: ",
+    determineSafety(input.reactorData),
+  );
+  console.timeEnd("Calculating Reactor Data Safety Without Problem Dampener");
+
+  console.time("Calculating Reactor Data Safety With Problem Dampener");
+  console.log(
+    "Safety of reactor data with problem dampener: ",
+    determineSafetyWithProblemDampener(input.reactorData),
+  );
+  console.timeEnd("Calculating Reactor Data Safety With Problem Dampener");
 }
--- src/02/index_test.ts
+++ src/02/index_test.ts
@@ -1,5 +1,8 @@
 import { assertEquals } from "@std/assert";
-import { determineSafety } from "./index.ts";
+import {
+  determineSafety,
+  determineSafetyWithProblemDampener,
+} from "./index.ts";
 
 Deno.test(function safetyTest() {
   const reactorData = [
@@ -12,4 +15,17 @@ Deno.test(function safetyTest() {
   ];
 
   assertEquals(determineSafety(reactorData), 2);
+});
+
+Deno.test(function safetyWithProblemDampenerTest() {
+  const reactorData = [
+    [7, 6, 4, 2, 1],
+    [1, 2, 7, 8, 9],
+    [9, 7, 6, 2, 1],
+    [1, 3, 2, 4, 5],
+    [8, 6, 4, 4, 1],
+    [1, 3, 6, 7, 9],
+  ];
+
+  assertEquals(determineSafetyWithProblemDampener(reactorData), 4);
 });
